6e18a81e6bf1fc1bdc66d0eee041ac6a3bc3afe0
Maddi Sears <maddisears@Maddis-MacBook-Pro-2.local>
2026-04-28 12:10:54 -0400

    more

--- project3_repository/athlete-recovery-app/src/DailyCheckInForm.js
+++ project3_repository/athlete-recovery-app/src/DailyCheckInForm.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { db } from './firebase';
-import { calculateReadinessScore, getReadinessStatus, getRecoveryMessage } from "./readinessCalculator";
 import { collection, addDoc, getDocs } from 'firebase/firestore';
 
 import {
